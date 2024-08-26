58a809f685b8bce4fbab5e2497ff375d449982d1
Harshit Kumar <harshitkumar9030@gmail.com>
2024-08-27 01:27:56 +0530

    included jwt with supabase

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
     images: {
         dangerouslyAllowSVG: true,
-        domains: ["res.cloudinary.com", "images.unsplash.com", "upload.wikimedia.org", "document-export.canva.com", "api.dicebear.com", "plus.unsplash.com"],
+        domains: ["res.cloudinary.com", "images.unsplash.com", "upload.wikimedia.org", "document-export.canva.com", "api.dicebear.com", "plus.unsplash.com", "dthezntil550i.cloudfront.net"],
     },
 
 };
--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -1,14 +1,29 @@
-import { createServerClient, type CookieOptions } from '@supabase/ssr'
-import { cookies } from 'next/headers'
-import { Database } from "@/types/database.types";
+import { createServerClient } from '@supabase/ssr';
+import { cookies } from 'next/headers';
+import { Database } from '@/types/database.types';
+import jwt from 'jsonwebtoken';
 
-export const createClient = () => {
+export const createClient = (supabaseId: string) => {
   const cookieStore = cookies();
+
+  if (!supabaseId) {
+    throw new Error('Supabase ID is required to create the Supabase client.');
+  }
+
+  const token = jwt.sign(
+    { sub: supabaseId }, // Sign the Supabase ID
+    process.env.NEXT_PUBLIC_SUPABASE_JWT_SECRET!
+  );
 
   return createServerClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
+      global: {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      },
       cookies: {
         getAll() {
           return cookieStore.getAll();
